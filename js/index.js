9f12d00f83b6813cd1e58efe228d5b4c9bf2e87f
Peter Breissinger <pbreissinger@gmail.com>
2018-10-14 13:22:29 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ particlesJS("particles-js", {
           "size":{"value":3,"random":true,"anim":{
             "enable":false,"speed":4,"size_min":0.3,"sync":false}},
             "line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},
-            "move":{"enable":true,"speed":4,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{
+            "move":{"enable":true,"speed":2.5,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{
               "enable":false,"rotateX":600,"rotateY":600}}},
               "interactivity":{"detect_on":"canvas","events":{
                 "onhover":{"enable":true,"mode":"bubble"},
